b38758f36228665a180a809c2c19bb32465b9bcb
Maverick <bredren10@gmail.com>
2019-06-17 00:59:31 +0100

    Add Product to SQL DB

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,72 +1,76 @@
 const Product = require('../models/product')
 
-exports.getAddProduct = (req,res,next) => {
+exports.getAddProduct = (req, res, next) => {
 	res.render('admin/add-product', {
 		pageTitle: 'Add Product',
 		activeLink: '/admin/add-product'
-	});	
+	});
 }
 
-exports.postAddProduct = (req,res,next) => {
-    const post = req.body;
-    const name = post.name;
-    const price = post.price;
-    const description = post.description;
-    const imageUrl = post.imageUrl;
-    const product = new Product(null,name,price,description,imageUrl);
-	product.save();
-	res.redirect('/');
+exports.postAddProduct = (req, res, next) => {
+	const post = req.body;
+	const name = post.name;
+	const price = post.price;
+	const description = post.description;
+	const imageUrl = post.imageUrl;
+	const product = new Product(null, name, price, description, imageUrl);
+	product.save()
+		.then(() => {
+			return res.redirect('/')
+		})
+		.catch(err => console.log(err));
+
+
 }
 
 
-exports.getEditProduct = (req,res,next) => {
+exports.getEditProduct = (req, res, next) => {
 
 	const productID = req.params.id;
 
-	Product.findById(productID,product => {
+	Product.findById(productID, product => {
 		res.render('admin/edit-product', {
 			pageTitle: 'Edit Product',
 			activeLink: '/admin/edit-product',
 			product
-		});	
+		});
 	});
 }
 
-exports.postEditProduct = (req,res,next) => {
+exports.postEditProduct = (req, res, next) => {
 
 
 	const productId = req.body.productId;
-	
+
 	newName = req.body.name;
 	newPrice = req.body.price;
 	newDescription = req.body.description;
 	newImageUrl = req.body.imageUrl;
 
-	const updatedProduct = new Product(productId,newName,newPrice,newDescription,newImageUrl);
+	const updatedProduct = new Product(productId, newName, newPrice, newDescription, newImageUrl);
 
 	console.log(updatedProduct.save());
-	
+
 	res.redirect(`/products#${ productId }`);
 
 }
 
 
-exports.getProducts  = (req,res,next) => {
-	Product.fetchAll( products => 
+exports.getProducts = (req, res, next) => {
+	Product.fetchAll(products =>
 		res.render('admin/products', {
 			products,
 			pageTitle: 'Admin Products',
 			activeLink: '/admin/products'
-		}
-	));
+		}));
 }
 
 
-exports.postDeleteProduct = (req,res,next) => {
+exports.postDeleteProduct = (req, res, next) => {
 
 	const productId = req.body.productId;
 
-	
+
 	Product.deleteById(productId);
 
 	res.redirect('/admin/products');
--- models/product.js
+++ models/product.js
@@ -2,30 +2,34 @@ const db = require('../util/db');
 
 
 module.exports = class Product {
-    constructor(id,name,price,description,imageUrl) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.imageUrl = imageUrl;
-    }
+  constructor(id, name, price, description, imageUrl) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
 
 
-    save() {
+  save() {
 
-    }
-
-    static fetchAll() {
-      return db.execute("SELECT * FROM `products`");
-    }
-
-    static findById(id) {
-
-    }
+    return db.execute("INSERT INTO `products` SET `name` = ?, `price` = ?, `description` = ?, `imageUrl` = ?", [this.name, this.price, this.description, this.imageUrl]);
 
 
-    static deleteById(id) {
 
-    }
+  }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM `products`");
+  }
+
+  static findById(id) {
+
+  }
+
+
+  static deleteById(id) {
+
+  }
 
 }
