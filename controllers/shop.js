58bcfe4e58cca95d1a25c2370fe04b996b940fa4
Maverick <bredren10@gmail.com>
2019-07-12 19:49:22 +0100

    Delete from Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,6 +62,30 @@ exports.getCart = (req, res, next) => {
 		.catch(err => console.log(err));
 }
 
+
+exports.postCartDeleteProduct = (req, res, next) => {
+	const productId = req.body.productId;
+	req.user
+		.getCart()
+		.then(cart => {
+			return cart.getProducts({
+				where: {
+					id: productId
+				}
+			});
+		})
+		.then(products => {
+			const product = products[0];
+			return product.cartItem.destroy();
+		})
+		.then(result => {
+			res.redirect('/cart');
+		})
+		.catch(err => console.log(err));
+};
+
+
+
 exports.postCart = (req, res, next) => {
 	const productId = req.body.productId;
 	req.user.getCart()
