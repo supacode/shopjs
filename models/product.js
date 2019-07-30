0224449f89e83ba1ee0a1f5c34e62c20c4304688
Maverick <bredren10@gmail.com>
2019-07-30 03:30:15 +0100

    productSchema for Mongoose

--- models/product.js
+++ models/product.js
@@ -1,69 +1,26 @@
-// class Product {
-//   constructor(name, price, description, imageUrl, id, userId) {
-//     this.name = name;
-//     this.price = +price;
-//     this.description = description;
-//     this.imageUrl = imageUrl;
-//     this._id = id ? new mongodb.ObjectId(id) : null;
-//     this.user = userId;
-//   }
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date
+  }
+});
 
-//   save() {
-//     const db = getDb();
-//     let action;
-
-//     if (this._id) {
-//       //  Update the Product
-//       action = db.collection('products').updateOne({
-//         _id: this._id
-//       }, {
-//         $set: this
-//       });
-//     } else {
-//       // Insert new Product
-//       action = db.collection('products').insertOne(this);
-//     }
-
-//     return action
-//       .then(result => result)
-//       .catch(err => {
-//         console.log(err)
-//       });
-//   }
-
-//   static fetchAll() {
-
-//     const db = getDb();
-
-//     return db.collection('products').find().toArray()
-//       .then(products => products)
-//       .catch(err => console.log(err));
-//   }
-
-//   static findById(_id) {
-//     const db = getDb();
-
-//     _id = new mongodb.ObjectId(_id); // Construct a new MongoDB ObjectId
-
-//     return db.collection('products').findOne({
-//         _id
-//       })
-//       .then(product => product)
-//       .catch(err => {
-//         console.log(err);
-//       })
-//   }
-
-//   static delete(_id) {
-//     const db = getDb();
-//     _id = new mongodb.ObjectId(_id); // Construct a new MongoDB ObjectId
-//     db.collection('products').deleteOne({
-//         _id
-//       })
-//       .then(result => result)
-//       .catch(err => console.log(err))
-//   }
-// }
-
-// module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
