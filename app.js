46b71054f7256d2655e0f7fd7d368fcd5c6e84eb
Maverick <bredren10@gmail.com>
2019-08-14 18:24:15 +0100

    Installed connect-mongodb-session

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const session = require("express-session");
 const User = require("./models/user");
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
+const MONGO_URI = 'mongodb://localhost/shop';
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+    bodyParser.urlencoded({
+        extended: false
+    })
 );
 
-app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+
+
+const store = new MongoStore({
+    uri: MONGO_URI,
+    collection: 'sessions'
+})
+
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store
+
+}));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -24,12 +39,12 @@ const authRoutes = require("./routes/auth");
 
 const errorRoutes = require("./controllers/error");
 app.use((req, res, next) => {
-  User.findOne()
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findOne()
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -38,24 +53,24 @@ app.use(authRoutes);
 app.use(errorRoutes.get404);
 
 mongoose
-  .connect("mongodb://localhost/shop", {
-    useNewUrlParser: 1
-  })
-  .then(() => {
-    User.findOne()
-      .then(user => {
-        if (!user) {
-          user = new User({
-            username: "Maverick",
-            email: "maverick@test.com",
-            cart: {
-              items: []
-            }
-          });
-          user.save();
-        }
-      })
-      .catch(err => console.log(err));
-    app.listen(3000);
-  })
-  .catch(err => console.log(err));
+    .connect(MONGO_URI, {
+        useNewUrlParser: 1
+    })
+    .then(() => {
+        User.findOne()
+            .then(user => {
+                if (!user) {
+                    user = new User({
+                        username: "Maverick",
+                        email: "maverick@test.com",
+                        cart: {
+                            items: []
+                        }
+                    });
+                    user.save();
+                }
+            })
+            .catch(err => console.log(err));
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
