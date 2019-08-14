d51d2e6dde04ff99caeacaf64f96447315aa14d5
Maverick <bredren10@gmail.com>
2019-08-14 18:06:52 +0100

    Installed connect-mongodb-session

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const session = require("express-session");
 const User = require("./models/user");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
+
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -20,38 +24,38 @@ const authRoutes = require("./routes/auth");
 
 const errorRoutes = require("./controllers/error");
 app.use((req, res, next) => {
-    User.findOne()
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findOne()
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
-
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 app.use(errorRoutes.get404);
 
-mongoose.connect('mongodb://localhost/shop', {
-        useNewUrlParser: 1
-    })
-    .then(() => {
-        User.findOne()
-            .then(user => {
-                if (!user) {
-                    user = new User({
-                        username: 'Maverick',
-                        email: 'maverick@test.com',
-                        cart: {
-                            items: []
-                        }
-                    });
-                    user.save();
-                }
-            })
-            .catch(err => console.log(err));
-        app.listen(3000);
-    })
-    .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost/shop", {
+    useNewUrlParser: 1
+  })
+  .then(() => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          user = new User({
+            username: "Maverick",
+            email: "maverick@test.com",
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+      .catch(err => console.log(err));
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
 exports.getLogin = (req, res, next) => {
+    console.log(req.session);
     res.render('auth/login', {
         activeLink: '/login',
         pageTitle: 'Login',
-        isAuth
+        isAuth: req.isAuth
     });
 }
 
 exports.postLogin = (req, res, next) => {
 
+    req.session.isLoggedIn = true;
+
+    res.redirect('/');
 
 }
